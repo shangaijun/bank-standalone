48a1ffbf1c33ec5e72325637754bca5ba4bbe858
shangaijun <shangaijun@yahoo.com.cn>
2014-11-25 00:02:45 +0800

    Accomplish the unlock module
    Signed-off-by: shangaijun <shangaijun@yahoo.com.cn>

--- unlock.py
+++ unlock.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python
 
-def unlock(personinfo):
-	f_personinfo = open(personinfo)
-	informations =f_personinfo.readlines()[1:]
+"""
+If your credit is locked,that means your credit's status is l or L
+This program could unlock your credit's status to unlock(U)
+"""
+
+def unlock(file):
+	new_informations = []
+	f_personinfo = open(file)
+	informations = f_personinfo.readlines()
 	f_personinfo.close()
+	
+	new_informations.append(informations[0].strip())
 
-	for infor in informations:
-		infor_s = infor.split()	
-		#print infor_s[4].lower()
+	for infor in informations[1:]:
+		infor = infor.strip()
+		infor_s = infor.split()
+
 		if infor_s[4].lower() == "l":
-			#print "you are locked"
+	#		print "you are locked"
 			infor_s[4] = "U"
-	
+			new_informations.append('\t'.join(infor_s))
+			continue
+		new_informations.append(infor)
 
-unlock('person.info')
+	f_personinfo = open(file,'w')
+	f_personinfo.write("\n".join(new_informations))
+	f_personinfo.flush()
+	f_personinfo.close()
 	
+	
+if __name__ == "__main__":
+	unlock('person.info')
